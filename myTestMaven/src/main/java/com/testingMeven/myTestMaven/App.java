c12ec4d9f843fd1daf5e419d7a1a1978d8571e0a
sachin <sachinbhute23nov@gmail.com>
2016-02-15 23:23:34 +0530

    first commit

---
+++ myTestMaven/src/main/java/com/testingMeven/myTestMaven/App.java
@@ -0,0 +1,21 @@
+package com.testingMeven.myTestMaven;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    @Test
+    public void testjava()
+    {
+    	System.out.println( "Hello World!" );
+    }
+}
---
+++ myTestMaven/src/test/java/com/testingMeven/myTestMaven/myjavaMevnclassTest.java
@@ -0,0 +1,13 @@
+package com.testingMeven.myTestMaven;
+
+import org.testng.annotations.Test;
+
+public class myjavaMevnclassTest {
+	
+	@Test
+	public void testjavam()
+	{
+		System.out.println("testing is best at all");
+	}
+
+}
