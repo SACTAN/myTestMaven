134302e547b29cd6f0e4214f032b9b7c4e6178a2
Sachin Bhute <sachinbhute23nov@gmail.com>
2016-02-17 22:27:28 +0530

    Added new java file

--- myTestMaven/src/test/java/com/testingMeven/myTestMaven/myjavaMevnclassTest.java
+++ myTestMaven/src/test/java/com/testingMeven/myTestMaven/myjavaMevnclassTest.java
@@ -1,5 +1,7 @@
 package com.testingMeven.myTestMaven;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class myjavaMevnclassTest {
@@ -9,6 +11,12 @@ public class myjavaMevnclassTest {
 	{
 		System.out.println("testing is best at all");
 		System.out.println("2nd commit");
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.eclipse.org/egit/download/");
+		String tile = driver.getTitle();
+		System.out.println("title of Page is :!" + tile);
+		
 	}
 
 }
