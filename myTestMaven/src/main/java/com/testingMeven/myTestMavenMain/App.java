3a5af9e71d2acd329b66010c04488ce22e4c04ea
Sachin Bhute <sachinbhute23nov@gmail.com>
2016-02-21 11:26:26 +0530

    Added modified app file

---
+++ myTestMaven/src/main/java/com/testingMeven/myTestMavenMain/App.java
@@ -0,0 +1,14 @@
+package com.testingMeven.myTestMavenMain;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public void testjava()
+    {
+    	System.out.println( "Hello World!" );
+    }
+}
